1b65327e9215edb20a6224336dc30099696524e1
zyy <939740415@qq.com>
2022-05-12 21:25:00 +0800

    提交代码

---
+++ 20220501/20220501/20220501-1.c
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h> //std-标准 standard input output
+//int
+//main前面的int表示main调用之后返回一个整型值
+int main() //主函数-程序入口
+{
+	//在这里完成任务
+	//库函数-调用头文件
+	//int a;
+	//scanf("%d", &a);
+	//printf("%d\n",a) ;
+	printf("hehh\n");
+	return 0; //返回0
+
+    //viod main（）已经过时
+    //数据类型
+	//char ch = 'A';//字符类型
+	//printf("%c\n", ch);
+
+
+}
---
+++ 20220501/20220501/20220501.cpp
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+int main()
+{
+	int a;
+	scanf("%d",&a);
+	printf("a,%d \n");
+	return 0;
+}
+
